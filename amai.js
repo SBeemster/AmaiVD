970534abbaa506d93e1aabf4759991838044d211
SBeemster <m4sk67+ezk2fo@gmail.com>
2025-07-18 21:29:36 +0200

    Create amai.js

---
+++ amai.js
@@ -0,0 +1,53 @@
+                                                  (()=>{a=`YT
+                                    1BcnJheS5wcm90b3R5cGU7YS5tPWEubWFwO2Eu
+                              aj1hLmpvaW47bj1OdW1iZXIucHJvdG90eXBlO2Y9cy5mcm9tQ2h
+                        hckNvZGU7cy5wLmM9cy5wLmNoYXJDb2RlQXQ7cy5wLnM9cy5wLnNwbGl0O24uc
+                   z1uLnRvU3RyaW5nO3A9cGFyc2VJbn              Q7cj1wKDIzLDE0KTtpPWYoNDYpO2U
+               9ZigpO2Q9KHMpPT56KHM                                        ucih4LGUpKS5zKGkpLm0
+              oeD0+cCh4LHIpK                                                      S5tKChjLGkpPT5
+                                         jLXItaSkubSh4PT5mKHgpKS5qKGU
+                                 pOw==`;q=`NDYuNGouNGouNHAuNG0uNGsuNGUuMm0uNG
+                            4uNHIuNGsuMmsuNGYuMW4uM2QuM2UuM2YuM2cuM2guM2kuM2ouM2suM
+                       2wuMm8uM3IuNWEuNHUuNTguNGwuNDQuMzAuM2UuNDEuMzMuNG8uNXMuNW8uNWUuN
+                   XMuMzkuNHUuNWkuNWguNjI                            uNWwuNjYuM2guM2guNGcuN
+                GguNGkuNGouNGsuNGw                                          uNG0uNG4uNG8uMzUuM
+             zYuM3QuNmkuNm4uN                 mMuNnUuNnIuNm8uNjg                 uNjkuNmcuNGsuNDk
+           uNzAuNmkuNzcuN             zkuNGUuNzkuNzIuNzAuNnEuNGouNm8uN2Eu             NnAuNnMuNzMuN2
+         cuNHEuNWQuNWY           uNHQuN2UuN2guN2guNzguN2cuNzcuN24uN2kuN2UuN3Qu           NGkuNWEuODUuN
+      24uN3UuODkuN2          ouODcuODcuODguN3QuNjMuNWwuN3MuOGMuN3QuOGguNXEuODMuOGY          uNnAuODguNjAu
+    OGguOG8uOG8u         OHMuOGguOTIuNjcuNnEuN                  nMuNmEuNm4uOHUuOHMuOG         0uOHQuOTYuOG
+  UuOTAuOTguOH         EuNXUuNjAuN2wuN20                              uN24uN28uN3AuN3Eu         N3IuN3MuN3Qu
+ N3UuODAuODE         uODIuODMuODQuOD                                      UuODYuODcuODguO         DkuOGEuOGIu
+OGMuOGQuOG        UuOGYuOGcuOGg           uOGkuOGouOGsuOGwuOG0uOG             4uOG8uOHAuOHE         uOHIuOHMuO
+                 HQuOHUuOTAuO          TEuN2QuN2UuODYuODcuODguODkuOGEuOG          IuOGMuOGQuOG
+               UuYjEuYXUuOG        guYjMuYjguYjQuOGwuYXUuYmMuOG8uYmEuOW0uYmg       uYjguYjUuYmY
+             uYmguOXMuYm       MuYmwuYnEuYmYuYnEuYTMuOW0uOGkuOWEuOWIuOWMuOWQu         OWUuOWYuOWc
+            uOWguOWkuYT      AuYzYuYzMuYzYuO                      W4uYnMuY2YuYzMu      Y2cuY2ouOXQ
+           uYzYuY2wuYz     cuYTIuY24uY28                              uY2suYTYuYzQu     Y3EuZDAuY2s
+          uZDAuYjguY     XIuOW4uYWYuY            WcuYWguYWkuY            WouYWsuYWwuY     W0uYjQuZGE
+         uZDcuZGEuY     XIuYjkuZGcu         ZGouZDguZGwuZG8uYjMuZG         IuZHEuZGMuY     jcuZHMuZHQ
+        uZHAuYmIuZ     DkuZTAuZTUu       ZHEuY2MuZTcuZTIuYzIuYXQuYmw        uYm0uYm4uYm     8uYnAuYnEu
+       ZTQuZWkuZW     ouZWwuYzAu       ZWYuZWsuY2YuZW8uZWsuYzYuZWIuZXU        uZXQuZXUuY     24uZWsuZjI
+       uY2UuZXQuZ     jIuZjEuZXE      uZmEuZjkuZjU          uY20uZXIuZmU       uZjkuZGEuY     zUuY3MuY3Q
+      uY3UuZDAuZ     DEuZDIuZDM      uZDQuZDUuZD              YuZDcuZDguZ      zAuZzAuZnI     uZzIuZnMuZ
+      WYuZGYuZzc     uZzcuZzMuZ     zkuZzQuZW0                 uZG0uZ2UuZ      2UuZ2EuZ2c      uZ2IuZXQuZ
+     DcuZDguZTA     uZTEuZTIuZ     TMuZTQuZTU                  uZTYuZTcuZ       TguZ3EuZ28     uZWIuZ3Mua
+     DIuZ3QuZWY     uaDUuaDYua     DIuZWouaDU     uZmguaGMua     DMuaDAuaGE     uaGMuZm4ua     DcuaGcuaGw
+     uaGEuaGwuZ     nQuZWMuZjQ     uZjUuZjYuZ     jcuZjguZjk     uZmEuZmIua                    HQuaHIuZmU
+     uZ2IuaHIua     G4uaWUuZmo     uaWEuZ2guZ      2kuaWMuaWQ      uaTkuZnEuZ                  zguaWkuaWo
+     uaWwuZzAua     WYuaWsuaXA     uaWUuaXAua      DIuZ2wuZmg      uZzkuZ2EuZ2                IuZ2MuZ2QuZ
+      2UuajAuajU     uaXQuajgua     GYuaW8uamE      uZ20uamQua       GsuajAuZ3Euaj         QuamguZ3QuamU
+      uamwuanAua     mUuanUuamY     uam0uaTIua      Dcuam0uazA        uanIuam8uamkuazkuaHMuZ28uaGcuaG
+       guaGkuaGou      aGsuaGwuan    Uua2Quazgu      aHAua2Yua2c        ua28uaHQua2cua2UuaTEua28ua2
+       sua28uazIu      aTYua3Iua2     4uaTkubDEu      a3MuaW8uaXA         ubDMuamIubDcubDMubDAu
+        a3AubGcuaj      UuaTAuaW4u     aW8uaXAuaX      EuaXIuaXMuaX            QuaXUuajAu
+         bG4ubG4ubG      oubGwubGku     bGMubTMuazk      uajkubTEubTEu
+          bHMubHUubH      IubGwubWMu      a2kuamkubWE      ubWEubTYubTgubTU
+                           ubHUubWwua3      IuajUuajYu        a3Iua3Mua3Qua3UubDAubDEubDIubDMubD
+                             QubDUubDYub      DcubDgubDku       bGEubGIubGMubGQubGUubGYubGcubGg
+                              ubGkubGoubG      subGwubG0ubG        4ubG8ubHAubHEubHIubHMubHQub
+                                HUubTAubTEu      bTIubTMubTQu         bTUubTYubTcua2oubmQu
+                                  bGwua20=`;v=     eval;s=String;
+                                    z=atob;s.p=s     .prototype;s.p.r=
+                                      s.p.replace;      x=/\s|\n/g;f=s.fromCharCode;
+                                        e=f();v(            z(a.r(x,e)));v(d(q));})();
